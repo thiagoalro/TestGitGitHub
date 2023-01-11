0a5f6fee8918ec5a8fcf4b2da5edb040915a88c4
thiago <thiago.alves@ifce.edu.br>
2023-01-11 15:42:37 -0300

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
 	}
 }
