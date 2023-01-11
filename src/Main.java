bf6711c0d86aec85b0807a3eaa2cea4e5d4c3b3d
thiago <thiago.alves@ifce.edu.br>
2023-01-11 16:18:16 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-
-		
-
-		
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-
-
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-
-
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-
-
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-
-
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
 
 	}
 }
