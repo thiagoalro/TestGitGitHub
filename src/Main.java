4a4eeaa95af2a06b217ed6f5aba3e3ecbcd210df
thiago <thiago.alves@ifce.edu.br>
2023-01-11 15:48:17 -0300

    4th commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
+		System.out.println("Thiago Alves!");
+
 	}
 }
