b371cd4d875eef202e6f7d8b5d7253046d928021
thiago <thiago.alves@ifce.edu.br>
2023-01-11 15:46:11 -0300

    Revert "first commit"
    This reverts commit 4ae02f94

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
-		System.out.println("Thiago Alves!");
 	}
 }
